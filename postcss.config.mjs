ed6aa7100c2a37cd265ec195ba81ad9fde898133
Dildora Muslimova <faithpacifica@yahoo.com>
2024-10-24 02:01:32 +0500

    fix:ts config fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,7 +2,17 @@
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
 export default config;
+
+
+// module.exports = {
+//   plugins: {
+//     // Example: adding Tailwind CSS and Autoprefixer
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
